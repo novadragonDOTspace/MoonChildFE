c486200ea8d6a1f56fe580d09d105821f46c298c
Mors <morsgames8@gmail.com>
2026-05-07 16:48:37 +0300

    Kill the console window

--- Platform/App/Main.cpp
+++ Platform/App/Main.cpp
@@ -1,14 +1,30 @@
 #include "Host.h"
 
-int main(int argc, char** argv)
+static int TheBeginningOfANewAndExcitinglyDifferentStory()
 {
-    (void)argc;
-    (void)argv;
-
 #ifdef __EMSCRIPTEN__
     static Host host;
 #else
     Host host;
 #endif
+
     return host.Run();
 }
+
+#if defined(_WIN32) && !defined(__EMSCRIPTEN__)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return TheBeginningOfANewAndExcitinglyDifferentStory();
+}
+#else
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    return TheBeginningOfANewAndExcitinglyDifferentStory();
+}
+#endif
